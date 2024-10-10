6c35c03373c2d22a1c63c281d6a2d78da8331989
Mohammed Day <M.day3862@student.leedsbeckett.ac.uk>
2024-10-10 11:48:01 +0100

    Week 1 Pratical

---
+++ Week 1/Practical.py
@@ -0,0 +1,29 @@
+def Basic_printing():
+    print("The code has been executed")
+    print("Hi")
+    print("There")
+    print("General kenobi")
+    help(print)
+
+def Basic_Expressions():
+    print(45+20)
+    print(10+20-15)
+    print(10*5)
+    print(100/33)
+    print(100//33)
+    print(10**2)
+    print(10%3)
+
+def Operator_Precedence():
+    print(10+5*2)
+    print(10-5*10+5)
+    print(5*10**2)
+    print((10+5)*2)
+    print(10-5*(10+5))
+    print((5*10)**2)
+    print(12+(5*2+3))
+    print(12+(5*(2+3)))
+
+#Basic_printing()
+#Basic_Expressions()
+Operator_Precedence()
